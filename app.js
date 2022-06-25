fcae0120cb37cac99eed1716a811602303887292
Prajakta Bhoite <p.bhoite@zensar.com>
2022-06-25 13:22:49 +0530

    app

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const PORT = 4000;
 
-var app = express();
+const apiRouter = require("./routes/apis");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(logger("dev"));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect("mongodb://localhost:27017/miniprojectbackend")
+.then(res => console.log(`Mongoose connected to db successfully ${res}`))
+.catch(err => console.log(`Mongoose connection to db failed ${err.message}`));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api",apiRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+console.log("let's start")
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Server is connected.at adreess ${PORT}`);
+})
